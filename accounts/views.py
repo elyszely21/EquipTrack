ef7d26ffb1035cfc6bb70211d0b9882b6bb796bc
LAB22_USER ON WS09 <L22X13W09@LabHCI.net>
2026-07-01 13:08:40 +0800

    Initial Django project setup

---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def login_view(request):
+    return render(request, 'registration/login.html')
+
+
+def register_view(request):
+    return render(request, 'registration/register.html')
